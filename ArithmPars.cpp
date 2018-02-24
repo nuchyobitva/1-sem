8b39f8f39d82b8d6e13d38788639e39bc860e894
Vilen <vilenmoscow@gmail.com>
2018-02-24 13:42:33 +0300

    Create ArithmPars.cpp

---
+++ ArithmPars.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y;
+    char com,com2;
+    cin >>x >> com;
+    if ((com=='>')||(com=='<')){
+        cin>>com2>>y;
+    }else{cin>>y;}
+    if (com=='+'){      cout << x+y;}
+    else if (com=='-'){        cout << x-y;    }
+    else if (com=='*'){        cout << x*y;    }
+    else if (com=='/'){        cout << (double)x/y;    }
+    else if (com=='%'){        cout << x%y;    }
+    else if (com=='&'){        cout << (x & y);    }
+    else if (com=='|'){        cout << ( x|y);    }
+    else if (com=='^'){        cout << (x^y);    }
+    else if ((com=='<')&&(com2==com)){        cout << (x<<y);    }
+    else if ((com=='>')&&(com2==com)){        cout << (x>>y);    }
+    else {cout<<"ERROR";
+    }
+    cin.get();
+    return 0;
+}
