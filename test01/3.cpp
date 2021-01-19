5f979c4666f54d56ee24e52229bf0180b31e0f98
Vilen <vilenmoscow@gmail.com>
2021-01-19 15:40:58 +0300

    Update 3.cpp

--- test01/3.cpp
+++ test01/3.cpp
@@ -1,3 +1,4 @@
+//old version
 #include <iostream>
 #include <sstream>
 using namespace std;
@@ -54,3 +55,54 @@ int main() {
 	cin.get();
 	return 0;
 }
+//new version
+/*
+#include <iostream>
+#include <string>
+#include <sstream>
+void read(int mas[10]){
+  std::string s;
+  getline(std::cin,s);
+  std::istringstream chit(s);
+  for (int i=0;i<10;i++){
+    int r;
+    if(chit>>r) mas[i]=r;
+    else {
+      std::cout<<"An error has occurred while reading the input sequence.\n";
+      exit(0);
+    }
+  }
+}
+bool decr(int mas[10]){
+  for (int i=0;i<9;i++){
+    if (mas[i]>mas[i+1]) return 0;
+  }
+  return 1;
+}
+int main(){
+  int mas1[10],mas2[10],n,mas[20];
+  read(mas1);
+  read(mas2);
+  if(!decr(mas1)||!decr(mas2)){
+     std::cout<<"The input sequence must be non-decreasing.\n";
+     return 0;
+  }
+  {
+  int i=0,j=0;
+  for (int x=0;x<20;x++){
+    if (mas1[i]<mas2[j]){
+      mas[x]=mas1[i];
+      i++;
+    }
+    else{
+      mas[x]=mas2[j];
+      j++;
+    }
+  }
+  }
+  for (int x=0;x<20;x++){
+    std::cout<<mas[x]<<" ";
+  }
+  return 0;
+}
+*/
