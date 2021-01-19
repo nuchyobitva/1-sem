ace367a61bd0e03a55dd448d9831b606e71f9d6e
Vilen <vilenmoscow@gmail.com>
2021-01-19 15:24:58 +0300

    Update 2.cpp

--- test01/2.cpp
+++ test01/2.cpp
@@ -1,3 +1,4 @@
+// old version
 #include <iostream>
 #include <sstream>
 using namespace std;
@@ -35,3 +36,44 @@ int main(){
     	else cout<<"There is no such pair of numbers";
 	return 0;
 }
+//new version
+/*
+#include <iostream>
+#include <string>
+#include <sstream>
+void read(int mas[10]){
+  std::string s;
+  getline(std::cin,s);
+  std::istringstream chit(s);
+  for (int i=0;i<10;i++){
+    int r;
+    if(chit>>r) mas[i]=r;
+    else {
+      std::cout<<"An error has occurred while reading the input sequence.\n";
+      exit(0);
+    }
+  }
+}
+bool decr(int mas[10]){
+  for (int i=0;i<9;i++){
+    if (mas[i]>mas[i+1]) return 0;
+  }
+  return 1;
+}
+int main(){
+  int mas[10],n;
+  read(mas);
+  if(!decr(mas)){
+     std::cout<<"The input sequence must be non-decreasing.\n";
+     return 0;
+  }
+  std::cin>>n;
+  for (int i=0;i<10;i++){
+    for (int j=0;j<10;j++){
+      if (((mas[i]+mas[j])==n)&&(i!=j)){ std::cout<<mas[i]<<" "<<mas[j]; return 0;} 
+    }
+  }
+  std::cout<<"There is no such pair of numbers.\n";
+  return 0;
+}
+*/
